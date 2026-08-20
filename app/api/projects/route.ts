0e4a0316700498fd6139576aaf4a03ea97d5ad89
Anil <anilmende2002@gmail.com>
2026-08-20 16:59:59 +0530

    feat: add projects API

---
+++ app/api/projects/route.ts
@@ -0,0 +1,70 @@
+import { connectToDatabase } from "@/lib/mongodb"
+import Project from "@/models/Project";
+import { NextResponse } from "next/server";
+
+
+export const GET = async () => {
+
+    try {
+        // connecting to database
+        await connectToDatabase();
+
+        // get all documents from the mongodb
+        const projects = await Project.find().sort({ createdAt : -1 });
+
+        return NextResponse.json(
+            {
+                success : true,
+                data : projects
+            },
+            { status : 200 }
+        );
+
+    } catch (error) {
+        console.error("Get projects error:", error);
+
+        return NextResponse.json(
+            {
+                success : false,
+                message : "Failed to fetch projects"
+            },
+            { status : 500 }
+        );
+
+    }
+}
+
+export const POST = async (request : Request) => {
+
+    try {
+        await connectToDatabase();
+
+        const body = await request.json();
+
+        const project = await Project.create({
+            name : body.name,
+            description : body.description,
+            status : body.status
+        });
+
+        return NextResponse.json(
+            {
+                success : true,
+                message : "Project created successfully",
+                data : project
+            },
+            { status : 201 }
+        );
+
+    } catch (error) {
+        console.error("Creat project error:", error);
+
+        return NextResponse.json(
+            {
+                success : false,
+                message : "Failed to create project"
+            },
+            { status : 500 }
+        );
+    }
+}
